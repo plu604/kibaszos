3c58a9116d09922a5bb9dbe3442014210bb05a8e
Pacó <pacotest@testmail.com>
2023-06-26 15:18:55 +0200

    changes for publishing

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,6 @@ from flask import Flask
 from config import DevelopmentConfig, ProductionConfig
 
 app = Flask(__name__, template_folder="../website", static_folder="../website/static", static_url_path="/static")
-app.config.from_object(DevelopmentConfig)
+app.config.from_object(ProductionConfig)
 
 from application import routes
